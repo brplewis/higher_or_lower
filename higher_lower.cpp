fb8a731db7906f075c65f9e393184480f9346f27
bob <bobrplewis@gmail.com>
2021-03-21 19:59:20 +0000

    Add main functionality

--- higher_lower.cpp
+++ higher_lower.cpp
@@ -12,22 +12,21 @@ whether the next random number is going
 to be higher or lower. 
 */
 
-// num_correct
+// Player and Game Variables 
+
+// Number of correct answers
 int num_correct = 0;
 
-// previous_card 
+// Previous card
 int previous_card = 0;
 
-//game_end
-bool game_end = false;
-
-//play_game
+// Whether to keep the game running
 bool play_game = true;
 
-int draw_card() { 
-    int card_num = rand() % 10;
-    return card_num;
-}
+// Player lost
+bool player_lost = false;
+
+// Game Functions 
 
 bool yes_or_no() {
     // Gets answer from user 
@@ -40,25 +39,101 @@ bool yes_or_no() {
     }
 }
 
-int make_a_guess() {
-    cout << "Make a guess! Choose a number from 1 - 10: " << endl;
-    cin >> guess;
+char make_a_guess() {
+    // Asks user for h or l and repeats 
+    // if an incorrect answer is given
+
+    while(true) {
+        string answer;
+        cin >> answer; 
+        if (answer.find('h') != string::npos) { 
+            char guess = 'h';
+            cout << "You choose higher! " << endl;
+            return guess;
+        } else if (answer.find('l') != string::npos) {
+            char guess = 'l';
+            cout << "You choose lower! " << endl;
+            return guess;
+        } else if (answer.find('s') != string::npos) {
+            char guess = 's';
+            cout << "You choose same! " << endl;
+            return guess;
+        } else {
+            cout << "Please make a guess by typing h for higher or l for lower." << endl;
+        }
+    }
 }
 
-bool play_round(guess) {
-    //
+int draw_card() { 
+    // Choose a random number between 1 & 10
+    srand((unsigned int)time(NULL));
+    int card_num = rand() % 10 + 1;
+    return card_num;
+}
+
+bool check_cards(int previous_card, int current_card, char player_guess) {
+    if (current_card < previous_card && player_guess == 'l') {
+        return true;
+    } else if (current_card > previous_card && player_guess == 'h') {
+        return true;
+    } else if (previous_card == current_card && player_guess == 's') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool play_round() {
+    cout << "========================================" << endl;
+    if (previous_card == 0) {
+        // Draw initial card 
+        previous_card = draw_card();
+        cout << "The first card drawn is a : " + to_string(previous_card) << endl;   
+    } else {
+        cout << "The last card was a : " + to_string(previous_card) << endl;
+    }
+     // Player makes a guess
+    cout << "Do you think the next card will be higher, lower or the same? (type h, l or s) " << endl;
+    char player_guess = make_a_guess();
+    cout << "Drawing a new card..." << endl;
+    int current_card = draw_card();
+    cout << "The card drawn is a : " + to_string(current_card) << endl;
+    if (check_cards(previous_card, current_card, player_guess)) {
+        // If the guess was correct
+        cout << "You guessed correctly!" << endl;
+        previous_card = current_card;
+        return true;
+    } else {
+        // If the guess was wrong
+        cout << "Bad luck!" << endl;
+        return false;
+    }
 }
 
 int main() {
     cout << "Higher or Lower!" << endl;
     while (play_game) { 
+        player_lost = false;
         cout << "Would you like to play? " << endl; 
         if (yes_or_no()) {
-            cout << draw_card();
-            cout << endl;
-            cout << "good game!" << endl;
+            while(!player_lost)
+                if (play_round()) {
+                    num_correct += 1; 
+                } else {
+                    cout << "You guessed " + to_string(num_correct) + " correctly";
+                    if(num_correct > 0) {
+                        cout << "!" << endl;
+                        if (num_correct > 10) {
+                            cout << "Well done!" << endl;
+                        }
+                    } else {
+                        cout << "." << endl;
+                    } 
+                    cout << "========================================" << endl;
+                    player_lost = true;
+                }
         } else {
-            //print thanks for playing 
+            cout << "Bye! Thanks for playing!" << endl;
             play_game = false;
         }
     }
